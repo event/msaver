ed94c0b3a2c1aeb91a598dc8ed1373a4e45e85f6
yuliya <yuliya@192.168.1.11>
2013-04-27 23:06:35 +0100

    finalize product and category sync

--- MSaver/src/org/movshovich/msaver/SyncMacher.java
+++ MSaver/src/org/movshovich/msaver/SyncMacher.java
@@ -7,6 +7,7 @@ import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.nio.MappedByteBuffer;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collection;
 import java.util.Date;
@@ -25,6 +26,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.stmt.UpdateBuilder;
 
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -82,8 +84,8 @@ public class SyncMacher extends AsyncTask<Void, Void, Long> {
 
 	private boolean isEmpty(String value, String hrName) {
 		if (value == null || value.isEmpty()) {
-			Toast.makeText(context, "Please fill in " + hrName
-					, Toast.LENGTH_SHORT).show();
+			Toast.makeText(context, "Please fill in " + hrName,
+					Toast.LENGTH_SHORT).show();
 			return true;
 		}
 		return false;
@@ -100,7 +102,7 @@ public class SyncMacher extends AsyncTask<Void, Void, Long> {
 		Date lastPush = new Date(lastPushTs);
 		json.put("username", username);
 		json.put("password", password);
-		
+
 		Collection<Transaction> txs;
 		SparseArray<Product> prods;
 		SparseArray<Category> cats;
@@ -120,7 +122,8 @@ public class SyncMacher extends AsyncTask<Void, Void, Long> {
 		return json;
 	}
 
-	private JSONArray categories2JsonArray(SparseArray<Category> cats) throws JSONException {
+	private JSONArray categories2JsonArray(SparseArray<Category> cats)
+			throws JSONException {
 		JSONArray jsonProds = new JSONArray();
 		int size = cats.size();
 		for (int i = 0; i < size; i += 1) {
@@ -129,7 +132,8 @@ public class SyncMacher extends AsyncTask<Void, Void, Long> {
 		return jsonProds;
 	}
 
-	private JSONArray products2JsonArray(SparseArray<Product> prods) throws JSONException {
+	private JSONArray products2JsonArray(SparseArray<Product> prods)
+			throws JSONException {
 		JSONArray jsonProds = new JSONArray();
 		int size = prods.size();
 		for (int i = 0; i < size; i += 1) {
@@ -173,9 +177,11 @@ public class SyncMacher extends AsyncTask<Void, Void, Long> {
 		return res;
 	}
 
-	private SparseArray<Category> getCategories(SparseArray<Product> prods) throws SQLException {
+	private SparseArray<Category> getCategories(SparseArray<Product> prods)
+			throws SQLException {
 		SparseArray<Category> res = new SparseArray<Category>();
-		Dao<Category,Integer> catDao = MainActivity.databaseHelper.getCategoryDao();
+		Dao<Category, Integer> catDao = MainActivity.databaseHelper
+				.getCategoryDao();
 		int size = prods.size();
 		for (int i = 0; i < size; i += 1) {
 			Category cat = prods.valueAt(i).getCategory();
@@ -187,9 +193,11 @@ public class SyncMacher extends AsyncTask<Void, Void, Long> {
 		return res;
 	}
 
-	private SparseArray<Product> getProducts(Collection<Transaction> txs) throws SQLException {
+	private SparseArray<Product> getProducts(Collection<Transaction> txs)
+			throws SQLException {
 		SparseArray<Product> res = new SparseArray<Product>();
-		Dao<Product, Integer> prodDao = MainActivity.databaseHelper.getProductDao();
+		Dao<Product, Integer> prodDao = MainActivity.databaseHelper
+				.getProductDao();
 		for (Transaction tx : txs) {
 			Product product = tx.getProduct();
 			if (res.get(product.getId()) == null) {
@@ -201,12 +209,13 @@ public class SyncMacher extends AsyncTask<Void, Void, Long> {
 	}
 
 	private List<Transaction> getTransaction(Date lastPush) throws SQLException {
-		Dao<Transaction, Integer> txDao = MainActivity.databaseHelper.getTransactionDao();
+		Dao<Transaction, Integer> txDao = MainActivity.databaseHelper
+				.getTransactionDao();
 		return txDao.queryBuilder().where().ge("date", lastPush).query();
 	}
 
 	private void processResponse(HttpResponse resp) throws IOException,
-			JSONException {
+			JSONException, SQLException {
 		InputStream content = resp.getEntity().getContent();
 		BufferedReader reader = new BufferedReader(new InputStreamReader(
 				content, "UTF-8"));
@@ -220,9 +229,106 @@ public class SyncMacher extends AsyncTask<Void, Void, Long> {
 		processResponse(json);
 	}
 
-	private void processResponse(JSONObject json) {
-		// TODO new json contains product list. It should be processed
+	private void processResponse(JSONObject json) throws JSONException,
+			SQLException {
+		String errorStr = json.getString("error");
+		if (errorStr != null) {
+			Toast.makeText(context, "Sync failed: " + errorStr, Toast.LENGTH_SHORT).show();
+			return;
+		}
+		JSONArray prods = json.getJSONArray("products");
+		JSONArray cats = json.getJSONArray("categories");
+		JSONArray shopList = json.getJSONArray("shoppingList");
+		SparseArray<Category> newCats = processCategories(cats);
+		SparseArray<Product> newProds = processProducts(prods, newCats);
+		showShoppingList(shopList, newProds);
+	}
 
+	private void showShoppingList(JSONArray shopList, SparseArray<Product> newProds) throws JSONException, SQLException {
+		Dao<Product, Integer> prodDao = MainActivity.databaseHelper
+				.getProductDao();
+		int length = shopList.length();
+		List<Product> list = new ArrayList<Product>(length);
+		for (int i = 0; i < length; i += 1) {
+			int itemId = shopList.getInt(i);
+			Product p;
+			if (itemId > 0) {
+				p = prodDao.queryForId(itemId);
+			} else {
+				p = newProds.get(itemId);
+			}
+			if (p == null) {
+				Log.w("MSaver", "Recieved shopping list with unknown porduct id " + itemId);
+			} else {
+				list.add(p);
+			}
+		}
+		
+	}
+
+	private SparseArray<Category> processCategories(JSONArray cats)
+			throws JSONException, SQLException {
+		int len = cats.length();
+		Dao<Category, Integer> catDao = MainActivity.databaseHelper
+				.getCategoryDao();
+		SparseArray<Category> newCats = new SparseArray<Category>();
+		for (int i = 0; i < len; i += 1) {
+			JSONObject obj = cats.getJSONObject(i);
+			int id = obj.getInt("id");
+			String nameVal = obj.getString("name");
+			if (id > 0) { // that's a name change for existing category
+				if (nameVal != null && !nameVal.isEmpty()) {
+					UpdateBuilder<Category, Integer> ub = catDao
+							.updateBuilder();
+					ub.where().idEq(id);
+					ub.updateColumnValue("name", nameVal);
+					ub.update();
+				}
+			} else { // create new categories
+				Category cat = new Category();
+				cat.setName(nameVal);
+				catDao.create(cat);
+				newCats.put(id, cat);
+			}
+		}
+		return newCats;
+	}
+
+	private SparseArray<Product> processProducts(JSONArray prods, SparseArray<Category> newCats)
+			throws JSONException, SQLException {
+		int length = prods.length();
+		Dao<Product, Integer> prodDao = MainActivity.databaseHelper
+				.getProductDao();
+		SparseArray<Product> newProds = new SparseArray<Product>();
+		for (int i = 0; i < length; i += 1) {
+			JSONObject obj = prods.getJSONObject(i);
+			int id = obj.getInt("id");
+			String nameVal = obj.getString("name");
+			if (id > 0) { // that's a name change for existing product
+				if (nameVal != null && !nameVal.isEmpty()) {
+					UpdateBuilder<Product, Integer> ub = prodDao
+							.updateBuilder();
+					ub.where().idEq(id);
+					ub.updateColumnValue("name", nameVal);
+					ub.update();
+				}
+			} else { // create new product
+				Product prod = new Product();
+				prod.setName(nameVal);
+				int catId = obj.getInt("catId");
+				Category cat;
+				if (catId > 0) {
+					cat = new Category();
+					cat.setId(catId);
+				} else {
+					cat = newCats.get(catId);
+				}
+				prod.setCategory(cat);
+				prodDao.create(prod);
+				newProds.put(id, prod);
+			}
+		}
+		return newProds;
 	}
 
 	@Override
